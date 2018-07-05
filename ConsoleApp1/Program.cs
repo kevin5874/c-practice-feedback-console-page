975cac7f3895b054bfea69b944fad3bf58c4d746
kevin5874 <39664646+kevin5874@users.noreply.github.com>
2018-07-05 05:58:14 -0500

    commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,59 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string courseName;
+            string castInput;
+            byte pageNumber;
+            bool needHelp;
+            string positiveExperiences;
+            string feedback;
+            int hoursStudiesd;
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What Course are you on");
+            courseName = Console.ReadLine();
+            Console.WriteLine("What page Number?");
+            castInput = Console.ReadLine();
+            try
+            {
+                pageNumber = Convert.ToByte(castInput);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid page number");
+                pageNumber = 0;
+            }
+            Console.WriteLine("Do you need any help? Yes or No");
+            castInput = Console.ReadLine();
+            if(castInput.ToLower() == "yes")
+            {
+                needHelp = true;
+            }
+            else if(castInput.ToLower() == "no")
+            {
+                needHelp = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid answer, please Email if you need help");
+                needHelp = false;
+            }
+            Console.WriteLine("Any positive experiences you'd like to share?");
+            positiveExperiences = Console.ReadLine();
+            Console.WriteLine("Any other feedback?");
+            feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study?");
+            castInput = Console.ReadLine();
+            try
+            {
+                hoursStudiesd = Convert.ToInt32(castInput);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            Console.ReadKey();
+
         }
     }
 }
